30c7980d34b001113831512847ea22afbc9043df
xxxx <xxx@xx.com>
2016-10-29 13:19:26 +0800

    Initial commit

---
+++ app/src/main/java/s07150730/intent/IntentDemo3.java
@@ -0,0 +1,14 @@
+package s07150730.intent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class IntentDemo3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
